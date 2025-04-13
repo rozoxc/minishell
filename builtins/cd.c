f4e756c9b1310f5432c541cb53b607c24a963174
Abderrahmane Abdoullah <ababdoul@e2r2p12.1337.ma>
2025-04-13 15:27:14 +0100

    add builtins functions

---
+++ builtins/cd.c
@@ -0,0 +1,114 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cd.c                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ababdoul <ababdoul@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/12 18:43:53 by ababdoul          #+#    #+#             */
+/*   Updated: 2025/04/13 14:10:49 by ababdoul         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/minishell.h"
+extern char **environ;  // Access environment variables
+
+void update_env_var(const char *key, const char *value)
+{
+    int i = 0;
+    char *env_entry;
+    char *new_env_entry;
+
+    // Search for existing env var (key=)
+    while (environ[i])
+    {
+        env_entry = environ[i];
+        if (strncmp(env_entry, key, strlen(key)) == 0 && env_entry[strlen(key)] == '=')
+        {
+            // Found the key, now update its value
+            new_env_entry = malloc(strlen(key) + strlen(value) + 2); // For key=value\0
+            if (!new_env_entry)
+            {
+                perror("malloc");
+                exit(1);
+            }
+            sprintf(new_env_entry, "%s=%s", key, value);
+            environ[i] = new_env_entry; // Update the env variable
+            return;
+        }
+        i++;
+    }
+
+    // If not found, add the new key=value entry
+    environ[i] = malloc(strlen(key) + strlen(value) + 2);
+    if (!environ[i])
+    {
+        perror("malloc");
+        exit(1);
+    }
+    sprintf(environ[i], "%s=%s", key, value);
+    environ[i + 1] = NULL;  // Ensure null-termination
+}
+
+int ft_cd(char **args)
+{
+    char old_pwd[1024];
+    char new_pwd[1024];
+
+    // Step 1: Check if argument exists
+    if (!args[1])
+    {
+        write(2, "cd: missing argument\n", 22);
+        return (1);
+    }
+
+    // Step 2: Save old PWD
+    if (getcwd(old_pwd, sizeof(old_pwd)) == NULL)
+    {
+        perror("getcwd");
+        return (1);
+    }
+
+    // Step 3: Try to change directory
+    if (chdir(args[1]) != 0)
+    {
+        perror("cd");
+        return (1);
+    }
+
+    // Step 4: Get the new PWD
+    if (getcwd(new_pwd, sizeof(new_pwd)) == NULL)
+    {
+        perror("getcwd");
+        return (1);
+    }
+
+    // Step 5: Update the environment variables
+    update_env_var("OLDPWD", old_pwd);
+    update_env_var("PWD", new_pwd);
+
+    return (0);
+}
+
+int main()
+{
+    char *args[] = {"cd", "/tmp", NULL};  // Test cd to /tmp
+    char *args_invalid[] = {"cd", "nonexistent", NULL};  // Test invalid path
+
+    printf("Testing ft_cd with valid path (cd /tmp):\n");
+    printf("Before cd:\n");
+    char cwd[1024];
+    if (getcwd(cwd, sizeof(cwd)) != NULL)
+        printf("Current directory: %s\n", cwd);
+
+    ft_cd(args);  // Call ft_cd with valid path
+
+    printf("After cd:\n");
+    if (getcwd(cwd, sizeof(cwd)) != NULL)
+        printf("Current directory: %s\n", cwd);
+
+    printf("\nTesting ft_cd with invalid path (cd nonexistent):\n");
+    ft_cd(args_invalid);  // Call ft_cd with invalid path
+    return 0;
+}
+
---
+++ builtins/echo.c
@@ -0,0 +1,88 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   echo.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ababdoul <ababdoul@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/12 17:39:34 by ababdoul          #+#    #+#             */
+/*   Updated: 2025/04/12 18:42:51 by ababdoul         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/minishell.h"
+
+void ft_putstr(char *str)
+{
+    int i;
+
+    i = 0;
+    while (str[i] != '\0')
+    {
+        write(1, &str[i] ,1);
+        i++;
+    }
+}
+int ft_strcmp1(char *s1, char *s2)
+{
+	int i = 0;
+
+	while((s1[i] == s2[i]) && s1[i] && s2[i])
+		i++;
+	return (s1[i]-s2[i]);
+}
+
+int ft_size(char **arg)
+{
+    int i;
+
+    i = 0;
+    while (arg[i] != NULL)
+        i++;
+    return (i);
+}
+
+int  ft_echo(char **arg)
+{
+    int no_new_line;
+    int size;
+    int i;
+
+    i = 1;
+    size = ft_size(arg);
+    no_new_line = 0;
+    if (arg[1] != NULL && ft_strcmp1(arg[1], "-n")  == 0)
+        no_new_line = 1;
+    if (no_new_line == 1)
+    {
+        i = 2;
+        while (i < size)
+        {
+            ft_putstr(arg[i]);
+            if (i < size - 1)
+                write(1, " ", 1);
+            i++;
+        }
+    }
+    else 
+    {
+        while (i < size)
+        {
+            ft_putstr(arg[i]);
+            if (i < size - 1)
+                write(1, " ", 1);
+            i++;
+        }
+        write(1, "\n", 1);
+    }
+    return (EXIT_SUCCESS);
+}
+
+// int main()
+// {
+//     ft_echo((char *[]){"echo", NULL}); // prints \n
+//     ft_echo((char *[]){"echo", "hello", NULL}); // hello\n
+//     ft_echo((char *[]){"echo", "-n", "hello", NULL}); // hello
+//     ft_echo((char *[]){"echo", "-n", NULL}); // (prints nothing)
+
+// }
