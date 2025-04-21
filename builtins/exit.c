1448c1f41ac10a72a8f8bc4104f4139aa178d180
Abderrahmane Abdoullah <ababdoul@e2r2p6.1337.ma>
2025-04-21 22:42:24 +0100

    fix exit

--- builtins/exit.c
+++ builtins/exit.c
@@ -6,14 +6,43 @@
 /*   By: ababdoul <ababdoul@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/14 18:23:14 by ababdoul          #+#    #+#             */
-/*   Updated: 2025/04/20 16:40:23 by ababdoul         ###   ########.fr       */
+/*   Updated: 2025/04/21 22:41:46 by ababdoul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-int ft_exit(void )
+static int ft_is_numeric(char *str)
 {
+    if (*str == '+' || *str == '-')
+        str++;
+    while (*str)
+    {
+        if (!ft_isdigit(*str))
+            return (0);
+        str++;
+    }
+    return (1);
+}
+int ft_exit(char **args)
+{
+    int exit_status;
+
     printf("exit\n");
-    exit(SUCCESS);    
+    if (args[1] == NULL)
+        exit(SUCCESS);
+    if (!ft_is_numeric(args[1]))
+    {
+        ft_putstr_fd("minishell: exit: ", STDERR_FILENO);
+        ft_putstr_fd(args[1], STDERR_FILENO);
+        ft_putstr_fd("numeric argument required", STDERR_FILENO);
+        exit(255);
+    }
+    if (args[2] != NULL)
+    {
+        ft_putstr_fd("bash: exit: too many arguments\n", STDERR_FILENO);
+        return (1);
+    }
+    exit_status = ft_atoi(args[1]);
+    exit(exit_status);
 }
