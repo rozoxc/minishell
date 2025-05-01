8677c68cb90b5769a0fd901cf3679fb23d248b3c
Abderrahmane Abdoullah <ababdoul@e3r10p3.1337.ma>
2025-05-01 22:28:39 +0100

    fix echo

--- builtins/echo.c
+++ builtins/echo.c
@@ -3,79 +3,73 @@
 /*                                                        :::      ::::::::   */
 /*   echo.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hfalati <hfalati@student.42.fr>            +#+  +:+       +#+        */
+/*   By: ababdoul <ababdoul@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/12 17:39:34 by ababdoul          #+#    #+#             */
-/*   Updated: 2025/04/27 22:06:41 by hfalati          ###   ########.fr       */
+/*   Updated: 2025/05/01 22:23:47 by ababdoul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-void	ft_putstr(char *str)
+int ft_size(char **arg)
 {
-	int	i;
-
-	i = 0;
-	while (str[i] != '\0')
-	{
-		write(1, &str[i], 1);
-		i++;
-	}
+    int i;
+    
+    i = 0;
+    while (arg[i] != NULL)
+        i++;
+    return (i);
 }
 
-int	ft_strcmp1(char *s1, char *s2)
+static void echo_print_args(char **arg, int start_idx, int size, int no_new_line)
 {
-	int	i;
-
-	i = 0;
-	while ((s1[i] == s2[i]) && s1[i] && s2[i])
-		i++;
-	return (s1[i] - s2[i]);
+    int i;
+    
+    i = start_idx;
+    while (i < size)
+    {
+        ft_putstr_fd(arg[i], 1);
+        if (i < size - 1)
+            write(1, " ", 1);
+        i++;
+    }
+    if (!no_new_line)
+        write(1, "\n", 1);
 }
 
-int	ft_size(char **arg)
+int check_flag(char *str)
 {
-	int	i;
-
-	i = 0;
-	while (arg[i] != NULL)
-		i++;
-	return (i);
+    int i;
+    
+    if (ft_strcmp(str, "-n") != 0 && ft_strncmp(str, "-n", 2) != 0)
+        return (0);
+    i = 2;
+    while (str[i] != '\0')
+    {
+        if (str[i] != 'n')
+            return (0);
+        i++;
+    }
+    return (1);
 }
 
-static void	echo_print_args(char **arg, int start_idx, \
-	int size, int no_new_line)
+int ft_echo(char **arg, t_obj *obj)
 {
-	int	i;
-
-	i = start_idx;
-	while (i < size)
-	{
-		ft_putstr(arg[i]);
-		if (i < size - 1)
-			write(1, " ", 1);
-		i++;
-	}
-	if (!no_new_line)
-		write(1, "\n", 1);
-}
-
-int	ft_echo(char **arg, t_obj *obj)
-{
-	int	no_new_line;
-	int	size;
-	int	i;
-
-	(void)obj;
-	i = 1;
-	size = ft_size(arg);
-	no_new_line = 0;
-	if (arg[1] != NULL && ft_strcmp1(arg[1], "-n") == 0)
-	{
-		no_new_line = 1;
-		i = 2;
-	}
-	echo_print_args(arg, i, size, no_new_line);
-	return (EXIT_SUCCESS);
+    int no_new_line;
+    int size;
+    int start_idx;
+    
+    (void)obj;
+    size = ft_size(arg);
+    no_new_line = 0;
+    start_idx = 1;
+    
+    while (start_idx < size && check_flag(arg[start_idx]))
+    {
+        no_new_line = 1;
+        start_idx++;
+    }
+    echo_print_args(arg, start_idx, size, no_new_line);
+    return (EXIT_SUCCESS);
 }
